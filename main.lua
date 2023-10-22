6b9481776e68c697a59fc6d44261d50ba62cda03
Thomas De Maen <mr-sylar59@world-of-sylar.fr>
2023-10-22 11:38:14 +0200

    use loop with fixed updates

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ Camera  = require("libs.hump.camera")
 Signal  = require("libs.hump.signal")
 M = require("libs.moses")
 
-function love.load()
+function love.load(args, uArgs)
     local classFiles = {}
     RecursiveEnumerate("objects", classFiles)
     RecursiveEnumerate("rooms", classFiles)
@@ -80,5 +80,50 @@ function RequireFiles(files)
     for _, file in pairs(files) do
         local file = file:sub(1, -5)
         require(file)
+    end
+end
+
+-- MAIN LOOP
+function love.run()
+    if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
+    if not love.timer then error("Timer module required") end
+
+    local lag = 0
+    local fixedDt = 1/60 -- DT fix (60 FPS)
+
+    -- Main loop time.
+    return function()
+        -- Process events.
+        if love.event then
+            love.event.pump()
+            for name, a,b,c,d,e,f in love.event.poll() do
+                if name == "quit" then
+                    if not love.quit or not love.quit() then
+                        return a or 0
+                    end
+                end
+                love.handlers[name](a,b,c,d,e,f)
+            end
+        end
+
+        local elapsed = love.timer.step()
+        lag = lag + elapsed
+
+        -- Call update and draw
+        while lag >= fixedDt do
+            if love.update then love.update(fixedDt) end
+            lag = lag - fixedDt
+        end
+
+        if love.graphics and love.graphics.isActive() then
+            love.graphics.origin()
+            love.graphics.clear(love.graphics.getBackgroundColor())
+
+            if love.draw then love.draw() end
+
+            love.graphics.present()
+        end
+
+        if love.timer then love.timer.sleep(0.001) end
     end
 end
